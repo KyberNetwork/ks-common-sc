ccf2185582a14d64c21e4c2814b8eacf750f0eba
Vu Nguyen Hoang <nghoangvu24@gmail.com>
2025-08-07 12:04:36 +0700

    chore: add deploy forwarder script

---
+++ script/DeployForwarder.s.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import './Base.s.sol';
+import 'src/KSGenericForwarder.sol';
+
+contract DeployForwarderScript is BaseScript {
+  string salt = '';
+
+  function run() public {
+    if (bytes(salt).length == 0) {
+      revert('salt is required');
+    }
+    salt = string.concat('KSGenericForwarder_', salt);
+
+    address forwarder =
+      _create3Deploy(keccak256(abi.encodePacked(salt)), type(KSGenericForwarder).creationCode);
+
+    _writeAddress('forwarder', forwarder);
+
+    emit DeployContract('KSGenericForwarder', forwarder);
+  }
+}
