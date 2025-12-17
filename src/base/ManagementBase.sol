33cad929c1d4679e8831ecddca8a9811795cb560
Vu Nguyen Hoang <nghoangvu24@gmail.com>
2025-12-17 15:54:28 +0700

    feat: add revoker roles

--- src/base/ManagementBase.sol
+++ src/base/ManagementBase.sol
@@ -10,16 +10,23 @@ import {
 } from 'openzeppelin-contracts/contracts/access/extensions/AccessControlDefaultAdminRules.sol';
 
 contract ManagementBase is AccessControlDefaultAdminRules, Common, IManagementBase {
-  modifier onlyRoleOrDefaultAdmin(bytes32 role) {
-    if (!hasRole(role, _msgSender()) && !hasRole(DEFAULT_ADMIN_ROLE, _msgSender())) {
-      revert AccessControlUnauthorizedAccount(_msgSender(), role);
-    }
-    _;
-  }
+  /// @inheritdoc IManagementBase
+  mapping(bytes32 role => bytes32 roleRevoker) public roleRevokers;
 
   constructor(uint48 initialDelay, address initialAdmin)
     AccessControlDefaultAdminRules(initialDelay, initialAdmin)
   {}
+
+  /// @notice Modifier that checks if the sender is either the given role or the default admin role
+  modifier onlyRoleOrDefaultAdmin(bytes32 role) {
+    if (!hasRole(role, _msgSender()) && !hasRole(DEFAULT_ADMIN_ROLE, _msgSender())) {
+      bytes32[] memory neededRoles = new bytes32[](2);
+      neededRoles[0] = role;
+      neededRoles[1] = DEFAULT_ADMIN_ROLE;
+      revert AccessControlUnauthorizedAccount(_msgSender(), neededRoles);
+    }
+    _;
+  }
 
   /// @inheritdoc IManagementBase
   function transferOwnership(address newOwner) external onlyRole(DEFAULT_ADMIN_ROLE) {
@@ -35,14 +42,6 @@ contract ManagementBase is AccessControlDefaultAdminRules, Common, IManagementBa
     _batchGrantRole(role, accounts);
   }
 
-  /// @dev See {batchGrantRole}
-  /// Internal function without access restriction.
-  function _batchGrantRole(bytes32 role, address[] memory accounts) internal {
-    for (uint256 i = 0; i < accounts.length; i++) {
-      _grantRole(role, accounts[i]);
-    }
-  }
-
   /// @inheritdoc IManagementBase
   function batchRevokeRole(bytes32 role, address[] memory accounts)
     external
@@ -51,11 +50,43 @@ contract ManagementBase is AccessControlDefaultAdminRules, Common, IManagementBa
     _batchRevokeRole(role, accounts);
   }
 
+  /// @inheritdoc IManagementBase
+  function setRoleRevoker(bytes32 role, bytes32 roleRevoker) external onlyRole(getRoleAdmin(role)) {
+    _setRoleRevoker(role, roleRevoker);
+  }
+
+  /// @notice Allows a role to be revoked by its revoker role
+  function revokeRole(bytes32 role, address account) public override {
+    bytes32[] memory neededRoles = new bytes32[](2);
+    neededRoles[0] = roleRevokers[role];
+    neededRoles[1] = getRoleAdmin(role);
+
+    if (!hasRole(neededRoles[0], _msgSender()) && !hasRole(neededRoles[1], _msgSender())) {
+      revert AccessControlUnauthorizedAccount(_msgSender(), neededRoles);
+    }
+
+    _revokeRole(role, account);
+  }
+
+  /// @dev See {batchGrantRole}
+  /// Internal function without access restriction.
+  function _batchGrantRole(bytes32 role, address[] memory accounts) internal {
+    for (uint256 i = 0; i < accounts.length; i++) {
+      _grantRole(role, accounts[i]);
+    }
+  }
+
   /// @dev See {batchRevokeRole}
   /// Internal function without access restriction.
   function _batchRevokeRole(bytes32 role, address[] memory accounts) internal {
     for (uint256 i = 0; i < accounts.length; i++) {
       _revokeRole(role, accounts[i]);
     }
+  }
+
+  function _setRoleRevoker(bytes32 role, bytes32 roleRevoker) internal {
+    bytes32 previousRoleRevoker = roleRevokers[role];
+    roleRevokers[role] = roleRevoker;
+    emit RoleRevokerChanged(role, previousRoleRevoker, roleRevoker);
   }
 }
--- src/interfaces/IManagementBase.sol
+++ src/interfaces/IManagementBase.sol
@@ -5,6 +5,13 @@ import {IERC1155} from 'openzeppelin-contracts/contracts/token/ERC1155/IERC1155.
 import {IERC721} from 'openzeppelin-contracts/contracts/token/ERC721/IERC721.sol';
 
 interface IManagementBase {
+  /// @notice Thrown when the account is missing one of the required roles
+  error AccessControlUnauthorizedAccount(address account, bytes32[] neededRoles);
+
+  event RoleRevokerChanged(
+    bytes32 indexed role, bytes32 indexed previousRoleRevoker, bytes32 indexed newRoleRevoker
+  );
+
   /// @notice Transfer {defaultAdmin} to a new account
   /// @dev Mimics the {Ownable-transferOwnership} function
   /// @param newOwner The new {defaultAdmin}
@@ -19,4 +26,12 @@ interface IManagementBase {
   /// @param role The role to revoke
   /// @param accounts The accounts to revoke the role from
   function batchRevokeRole(bytes32 role, address[] memory accounts) external;
+
+  /// @notice Returns the role other than the admin role that can revoke the given role
+  function roleRevokers(bytes32 role) external view returns (bytes32);
+
+  /// @notice Sets the revoker roles for multiple roles
+  /// @param role The role to update the revoker role for
+  /// @param roleRevoker The new revoker role for the role
+  function setRoleRevoker(bytes32 role, bytes32 roleRevoker) external;
 }
