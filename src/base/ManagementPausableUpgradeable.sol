70330a70a88a2ca5c4c4fab2cab16de9de7e6388
Vu Tran <vutt94@gmail.com>
2025-12-26 01:13:30 +0700

    feat: add managementPausable & managementRescuable upgradeable

---
+++ src/base/ManagementPausableUpgradeable.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IManagementPausable} from '../interfaces/IManagementPausable.sol';
+
+import {KSRoles} from '../libraries/KSRoles.sol';
+import {ManagementBaseUpgradeable} from './ManagementBaseUpgradeable.sol';
+
+import {
+  PausableUpgradeable
+} from 'openzeppelin-contracts-upgradeable/contracts/utils/PausableUpgradeable.sol';
+
+abstract contract ManagementPausableUpgradeable is
+  ManagementBaseUpgradeable,
+  IManagementPausable,
+  PausableUpgradeable
+{
+  constructor() {
+    _disableInitializers();
+  }
+
+  function initialize(address[] memory initialGuardians) public initializer {
+    __ManagementPauseable_init(initialGuardians);
+  }
+
+  function __ManagementPauseable_init(address[] memory initialGuardians) internal onlyInitializing {
+    _batchGrantRole(KSRoles.GUARDIAN_ROLE, initialGuardians);
+  }
+
+  /// @inheritdoc IManagementPausable
+  function pause() external onlyRoleOrDefaultAdmin(KSRoles.GUARDIAN_ROLE) {
+    _pause();
+  }
+
+  /// @inheritdoc IManagementPausable
+  function unpause() external onlyRole(DEFAULT_ADMIN_ROLE) {
+    _unpause();
+  }
+}
