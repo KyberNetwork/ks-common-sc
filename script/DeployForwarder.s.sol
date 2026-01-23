55355d2da0c58c487ebcb8a72c22aaaca6e7f0e0
Cuong Tran <tranquoccuong@outlook.com.vn>
2026-01-23 16:43:36 +0700

    feat: deploy Forwarder to BSC and Base

--- script/DeployForwarder.s.sol
+++ script/DeployForwarder.s.sol
@@ -5,7 +5,7 @@ import './Base.s.sol';
 import 'src/KSGenericForwarder.sol';
 
 contract DeployForwarderScript is BaseScript {
-  string salt = '';
+  string salt = '260123';
 
   function run() public {
     if (bytes(salt).length == 0) {
@@ -14,7 +14,7 @@ contract DeployForwarderScript is BaseScript {
     salt = string.concat('KSGenericForwarder_', salt);
 
     vm.startBroadcast();
-    address forwarder =
+    (address forwarder,) =
       _create3Deploy(keccak256(abi.encodePacked(salt)), type(KSGenericForwarder).creationCode);
     vm.stopBroadcast();
 
